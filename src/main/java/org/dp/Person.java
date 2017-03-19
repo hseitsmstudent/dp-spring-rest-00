1666df435320a748a9deee62d39e3dba0804f51a
hseitsmstudent <hse.itsm.student@gmail.com>
2017-03-19 21:33:24 +0300

    Mongo DB instead H2

--- src/main/java/org/dp/Person.java
+++ src/main/java/org/dp/Person.java
@@ -1,19 +1,22 @@
 package org.dp;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.*;
+//import javax.persistence.*;
 
 /**
  * Created by student on 3/6/17.
  */
 @Data
-@Entity
-@Table(name = "people")
+@Document
+//@Entity
+//@Table(name = "people")
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id; //long id;
 
     private String firstName;
     private String lastName;
--- src/main/java/org/dp/PersonRepository.java
+++ src/main/java/org/dp/PersonRepository.java
@@ -1,6 +1,7 @@
 package org.dp;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,6 +11,6 @@ import java.util.List;
  * Created by student on 3/6/17.
  */
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+public interface PersonRepository extends MongoRepository<Person, String> {//PagingAndSortingRepository<Person, Long> {
     List<Person> findByLastName(@Param("name") String name);
 }
